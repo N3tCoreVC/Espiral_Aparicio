87f47caacd47d8d81a163ac34e382ee0f14e09e3
unknown <apariciodaniel250@gmail.com>
2018-09-19 13:37:08 -0500

    Triangulo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Triangulo
+{
+    class Program
+    {
+
+        static int num = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el tamaño de la base: ");
+            if (Int32.TryParse(Console.ReadLine().ToString(), out num)) {
+                creaTriangulo(num);
+            } else {
+                Console.WriteLine("Debes ingresar un numero entero");
+            }
+        }
+
+        static void creaTriangulo(int b) {
+            String cadena = "";
+            for (int i=1; i<=b; i++) {
+                Console.Write(cadena.PadLeft(b-i, ' '));
+                for (int j=0; j<i; j++) {
+                    Console.Write("1 ");                    
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
